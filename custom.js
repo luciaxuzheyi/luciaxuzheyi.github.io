cfc7d93251d9c8eccd8ffd47f8c294c12dbb4e7b
luciaxuzheyi <luciaxuzheyi@outlook.com>
2022-05-27 12:33:40 +0800

    Site updated: 2022-05-27 12:33:31

--- custom.js
+++ custom.js
@@ -1,3 +1,3 @@
-hexo.extend.injector.register('body_end', `
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>hexo.extend.injector.register('body_end', `
   <script src="/js/custom.js"></script>
 `)
--- swiper/script.js
+++ swiper/script.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper(".swiper-container", {
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>var swiper = new Swiper(".swiper-container", {
   slidesPerView: 1,
   spaceBetween: 20,
   effect: "fade",
